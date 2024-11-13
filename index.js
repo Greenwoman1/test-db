921a12e536a32b527bae63859ee17d4e38e5a21e
sara <sara12travnik@gmail.com>
2024-11-13 09:36:56 +0100

    sa

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get('/redis-test', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
