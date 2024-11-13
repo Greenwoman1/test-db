8230c551eef90b752ba116889096a1ee3e8a0c16
sara <sara12travnik@gmail.com>
2024-11-13 09:15:26 +0100

    fds

--- src/ToponSKURule/ToponSKURule.js
+++ src/ToponSKURule/ToponSKURule.js
@@ -1,6 +1,5 @@
 const { DataTypes, Model, UUID, UUIDV4 } = require('sequelize');
 const sequelize = require('../../clients/sequelize');
-const { getSKUById } = require('../SKU/skuController');
 const SKU = require('../SKU/SKU');
 
 class ToponSKURule extends Model {
